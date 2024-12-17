c7db44a4e0d62d35e214bace9e150def5965819b
ShadyTiger <chatterjeesoumyajeet@gmail.com>
2024-12-18 00:24:00 +0530

    FIX: frontend index page design fixes

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -43,50 +43,55 @@ export default function Index() {
           return card.id !== newCard.id;
         })
       );
-      setResult((current: any) => ({ ...current, [swipe]: current[swipe] - 1 }));
+      setResult((current: any) => ({
+        ...current,
+        [swipe]: current[swipe] - 1
+      }));
       setCards((current) => [...current, newCard]);
     }
   };
 
   return (
-    <div className="relative flex flex-col justify-center items-center w-full h-screen gradient">
-      {/* <Head> */}
-        <title>Tinder cards with Framer motion</title>
-      {/* </Head> */}
-      <AnimatePresence>
-        {cards.map((card, index) => (
-          <Card
-            key={card.name}
-            active={index === activeIndex}
-            removeCard={removeCard}
-            card={card}
+    <main className="bg-blue-500 flex flex-col justify-center items-center py-10 max-h-screen">
+      <h2 className="text-white text-3xl font-semibold">Tinder cards with Framer motion</h2>
+      <div className="relative flex flex-col justify-center items-center w-full h-screen gradient">
+        {/* <Head> */}
+        {/* </Head> */}
+        <AnimatePresence>
+          {cards.map((card, index) => (
+            <Card
+              key={card.name}
+              active={index === activeIndex}
+              removeCard={removeCard}
+              card={card}
+            />
+          ))}
+        </AnimatePresence>
+        {cards.length === 0 ? (
+          <span className="text-white text-xl">End of Stack</span>
+        ) : null}
+        <footer className="absolute bottom-4 flex items-center space-x-4">
+          <div className="flex flex-col items-center space-y-2">
+            <button
+              disabled={history.length === 0}
+              className="w-14 h-14 rounded-full text-black bg-white inline-flex justify-center items-center disabled:cursor-not-allowed"
+              onClick={undoSwipe}
+              data-testid="undo-btn"
+              aria-label="Undo Swipe"
+            >
+              <RotateIcon strokeWidth={3} />
+            </button>
+            <span className="text-xs text-white">Undo</span>
+          </div>
+          <Counter label="Likes" count={result.like} testid="like-count" />
+          <Counter label="Nopes" count={result.nope} testid="nope-count" />
+          <Counter
+            label="Superlike"
+            count={result.superlike}
+            testid="superlike-count"
           />
-        ))}
-      </AnimatePresence>
-      {cards.length === 0 ? (
-        <span className="text-white text-xl">End of Stack</span>
-      ) : null}
-      <footer className="absolute bottom-4 flex items-center space-x-4">
-        <div className="flex flex-col items-center space-y-2">
-          <button
-            disabled={history.length === 0}
-            className="w-14 h-14 rounded-full text-black bg-white inline-flex justify-center items-center disabled:cursor-not-allowed"
-            onClick={undoSwipe}
-            data-testid="undo-btn"
-            aria-label="Undo Swipe"
-          >
-            <RotateIcon strokeWidth={3} />
-          </button>
-          <span className="text-xs text-white">Undo</span>
-        </div>
-        <Counter label="Likes" count={result.like} testid="like-count" />
-        <Counter label="Nopes" count={result.nope} testid="nope-count" />
-        <Counter
-          label="Superlike"
-          count={result.superlike}
-          testid="superlike-count"
-        />
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </main>
   );
 }
